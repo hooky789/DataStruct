3e7a9835e287ead81192d348da86099f1ce9d01d
robin <bzhou830@163.com>
2016-06-19 16:41:29 +0800

    add QuickSort.cpp

---
+++ others/QuickSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+void QuickSort(T a[], int l, int r)
+{
+	T tmp = a[l];
+	int i = l;
+	int j = r;
+	while(i < j)
+	{
+		while(i < j && a[j] >= tmp) --j;
+		if(i < j)
+		{
+			a[i] = a[j];
+			++i;
+		}
+		
+		while(i < j && a[i] < tmp) ++i;
+		if(i < j)
+		{
+			a[j] = a[i];
+			--j;
+		}
+	}
+	a[i] = tmp;
+	if(l < i-1)
+		QuickSort(a, l, i-1);
+	if(j+1 < r)
+		QuickSort(a, j+1, r);
+}
+
+int a[10] = {1,3,2,6,9,12,34,8,56,22};
+
+int main()
+{
+	
+	for(int i = 0; i < 10; ++i)
+		cout<<a[i]<<" ";
+	
+	cout<<endl;
+	QuickSort(a, 0, 9);
+
+	
+	for(int i = 0; i < 10; ++i)
+		cout<<a[i]<<" ";
+	
+	cout<<endl;
+	return 0;
+}
+
+
+
