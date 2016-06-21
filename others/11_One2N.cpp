f1b03ba9bf1122e9c99f973d35e68dd47094e28b
robin <chou_robin@hotmail.com>
2016-06-21 09:50:29 +0800

    Rec

--- others/11_One2N.cpp
+++ others/11_One2N.cpp
@@ -5,7 +5,6 @@
 	> Created Time: 2016年06月20日 星期一 16时31分07秒
     > From: 剑指offer
 ************************************************************************/
-
 #include<iostream>
 #include<string.h>
 #include<stdio.h>
@@ -69,10 +68,43 @@ void PrintOne2N(int n)
 	delete[] numbers;
 }
 
+//递归
+void Recursive(char* num, int length, int index)
+{
+    if(index == length-1)
+    {
+        PrintNumbers(num);
+        return;
+    }
+
+    for(int i=0; i<10; ++i)
+    {
+        num[index + 1] = i + '0';
+        Recursive(num, length, index + 1);
+    }
+
+}
+
+//递归的方法实现
+void RCPrintOne2N(int n)
+{
+    if(n <= 0) return;
+    char* numbers = new char[n + 1];
+    memset(numbers, '0', sizeof(char) * (n+1));
+    numbers[n] = 0;
+
+    for(int i=0; i<10; ++i)
+    {
+        numbers[0] = i + '0';
+        Recursive(numbers, n, 0);
+    }
+    delete[] numbers;
+}
+
 
 int main()
 {
-	PrintOne2N(3);
+	RCPrintOne2N(3);
     return 0;
 }
 
